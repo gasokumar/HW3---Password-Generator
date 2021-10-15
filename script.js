bc68b2603d321d1e481d5c2be6dafb40276b617b
Ami Asokumar <gasokumar@gmail.com>
2021-10-14 22:51:18 -0400

    Made prompts for length

--- script.js
+++ script.js
@@ -21,6 +21,25 @@ function writePassword() {
 
 // Create generatePassword();
 function generatePassword() {
+  var passLength = window.prompt(
+    "How long do you want your password to be? (8-128 characters!)"
+  );
+  var responseLength;
+
+  if (passLength.IsInteger == false) {
+    responseLength = "Must be an integer!";
+  } else if (passLength == 0) {
+    responseLength = "Can't be 0!";
+  } else if (passLength < 8) {
+    responseLength = "Needs to be longer!";
+  } else if (passLength > 128) {
+    responseLength = "Needs to be shorter!";
+  } else {
+    responseLength = "Your password will be " + passLength + " digits long";
+  }
+
+  window.alert(responseLength);
+
   var password = "asokumar";
   return password;
 }
@@ -31,3 +50,19 @@ function generatePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // INITIALIZATIONS (GETTING THINGS STARTED) ================
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
